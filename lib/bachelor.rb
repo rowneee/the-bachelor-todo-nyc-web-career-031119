29d6ecd8e6744cbe0ab7c520a6802dd147343ac4
Roni Shabo <roni.shabo@gmail.com>
2019-03-04 20:41:31 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -22,11 +22,16 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
+  count = 0
   data.each do |season, array|
     array.each do |people|
       people.each do |key, value|
         if value == hometown
-          
+          count += 1 
+        end
+      end
+    end
+  end
 end
 
 def get_occupation(data, hometown)
