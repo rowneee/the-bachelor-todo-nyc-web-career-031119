6193a18c12bda2d5626e1a38a4ffee0f1be109fc
Roni Shabo <roni.shabo@gmail.com>
2019-03-04 20:15:45 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,6 +2,8 @@ def get_first_name_of_season_winner(data, season)
   data.each do |key, value|
     if value == "Winner"
       return["name"]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
