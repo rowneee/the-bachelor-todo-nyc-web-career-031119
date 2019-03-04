ce07593f38348ed3c577e97f31f1e9d5eba52069
Roni Shabo <roni.shabo@gmail.com>
2019-03-04 20:52:39 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -54,7 +54,7 @@ def get_average_age_for_season(data, season)
     contestants.each do |key, value|
       if key == "age"
         value += value
-        return value / value.length - 1
+        return (value / value.length - 1)
       end
     end
   end
